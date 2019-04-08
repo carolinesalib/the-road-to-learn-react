2673c4485bb9dc80fa8df349fbe304c04a28e666
Caroline Salib <carolinesalibc@gmail.com>
2019-04-08 07:22:46 -0600

    Add hackernews project

---
+++ hackernews/src/App.js
@@ -0,0 +1,75 @@
+import React, { Component } from 'react';
+import './App.css';
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+  {
+    title: 'Test',
+    url: 'https://Test.js.org',
+    author: 'Noone',
+    num_comments: 2,
+    points: 5,
+    objectID: 2,
+  },
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list: list
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        {this.state.list.map(item => {
+          return (
+          <div key={item.objectID}>
+            <span>
+              <a href={item.url}>{item.title}</a>
+            </span>
+            <span>{item.author}</span>
+            <span>{item.num_comments}</span>
+            <span>{item.title}</span>
+            <span>
+              <button
+                onClick={() => this.onDismiss(item.objectID)}
+                type="button"
+              >
+                Dismiss
+              </button>
+            </span>
+          </div>);
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
