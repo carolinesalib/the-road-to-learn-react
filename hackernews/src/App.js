6f4631924d3560228e2cf9c5269e66d64411b05f
Caroline Salib <carolinesalibc@gmail.com>
2019-04-23 07:35:30 -0600

    Change input to a controlled component by adding the value attr

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -58,15 +58,17 @@ class App extends Component {
   }
   
   render() {
+    const { searchTerm, list } = this.state;
     return (
       <div className="App">
         <form>
           <input
             type="text"
             onChange={this.onSearchChange}
+            value={searchTerm}
           />
         </form>
-        {this.state.list.filter(isSearched(this.state.searchTerm)).map(item => {
+        {list.filter(isSearched(searchTerm)).map(item => {
           return (
           <div key={item.objectID}>
             <span>
